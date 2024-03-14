f70bc17329f0f69b73e76d75fa36e29eb1667d09
kyouvv <134747701+kyouvv@users.noreply.github.com>
2024-03-14 19:21:16 +0800

    api

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -20,7 +20,7 @@ function Landing() {
     // Function to fetch data from the API
     const fetchData = async () => {
         try {
-            const response = await fetch('https://jsonplaceholder.typicode.com/users');
+            const response = await fetch('https://pupcc-web.onrender.com/api/getorgs');
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
